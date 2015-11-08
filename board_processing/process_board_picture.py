9af1528fc3568824efa587f232480738b685dbbc
Andrew Sherman <watemerald@gmail.com>
2015-11-08 13:25:26 +0200

    Added basic command line interface

--- board_processing/process_board_picture.py
+++ board_processing/process_board_picture.py
@@ -1,5 +1,5 @@
 """
-Author: 
+Author:
 Vladimir Duchenchuk
 Bogdan Kulynych (hello@bogdankulynych.me)
 """
@@ -7,18 +7,25 @@ Bogdan Kulynych (hello@bogdankulynych.me)
 import math
 import numpy as np
 import cv2
+from docopt import docopt
 
 from aset import ApproximateSet
 
+__usage = """
+Process Board Image
+
+Usage:
+  process_board_picture [<image>]
+"""
 
 # http://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
 def order_points(pts):
     rect = np.zeros((4, 2), dtype = "float32")
-        
+
     s = pts.sum(axis = 1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
-        
+
     diff = np.diff(pts, axis = 1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
@@ -29,26 +36,26 @@ def order_points(pts):
 def four_point_transform(image, pts):
     rect = order_points(pts)
     (tl, tr, br, bl) = rect
-   
+
     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
     maxWidth = max(int(widthA), int(widthB))
-    
+
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
-        
- 
+
+
     dst = np.array([
                     [0, 0],
                     [maxWidth - 1, 0],
                     [maxWidth - 1, maxHeight - 1],
                     [0, maxHeight - 1]], dtype = "float32")
-                        
-    
+
+
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-   
+
     return warped
 
 
@@ -68,8 +75,8 @@ def process_board_picture(image_path, display_mode='show'):
         print('Failed to load image file:', image_path)
 
     # Load current state image
-    img2 = cv2.imread("/Users/niddhogg/Documents/board_processing/BoardProcessing/board_processing/test_images/IMG_3485.jpg")
-
+    # img2 = cv2.imread("/Users/niddhogg/Documents/board_processing/BoardProcessing/board_processing/test_images/IMG_3485.jpg")
+    img2 = cv2.imread("test_images/IMG_3485.jpg")
 
 
 
@@ -145,7 +152,7 @@ def process_board_picture(image_path, display_mode='show'):
         num = i * (size/8)
         cv2.line(img_dif_copy,(0,num),(size,num),(0,255,0),1)
         cv2.line(img_dif_copy,(num,0),(num,size),(0,255,0),1)
-    
+
     cv2.imshow("Original",original )
     cv2.imshow("Dif", img_dif_copy)
 
@@ -157,10 +164,10 @@ def process_board_picture(image_path, display_mode='show'):
         for j in range(0,8):
             start_x = i*(size/8)
             start_y = j*(size/8)
-            
+
             end_x = (i+1)*(size/8)
             end_y = (j+1)*(size/8)
-            
+
             sqr_img = img_dif[start_x:end_x, start_y:end_y].copy()
             # ret,thresh = cv2.threshold(sqr_img,70,255,cv2.THRESH_TOZERO)
             img_board.append(sqr_img)
@@ -182,7 +189,13 @@ def process_board_picture(image_path, display_mode='show'):
 
     cv2.waitKey(0)
 
-
-
 if __name__ == '__main__':
-    process_board_picture('/Users/niddhogg/Documents/board_processing/BoardProcessing/board_processing/test_images/IMG_3472.jpg')
+    # process_board_picture('/Users/niddhogg/Documents/board_processing/BoardProcessing/board_processing/test_images/IMG_3472.jpg')
+    arguments = docopt(__usage, version='Board Processing 0.0.1')
+
+    image = arguments['<image>']
+
+    if image is None:
+        image = 'test_images/IMG_3472.jpg'
+
+    process_board_picture(image)
